167941508fa568fbaa296f1bf766859752e69da6
Daniele Tramis <tramis@webscience.it>
2021-05-11 16:18:14 +0200

    OM-269 [tramis] unified method to validate both version of SRA and SUA

--- src/main/java/it/gruppopam/app_common/network/api/DeviceAppManagerApi.java
+++ src/main/java/it/gruppopam/app_common/network/api/DeviceAppManagerApi.java
@@ -10,17 +10,12 @@ import retrofit2.http.Query;
 
 public interface DeviceAppManagerApi {
 
-
     @GET("settings")
     Call<HashMap<String, String>> getSettings(@Query("store_id") Long storeId);
 
-    @GET("store_utilities_app/apk/validate")
-    Call<ResponseBody> validateApkVersion(@Query("current_version") Long currentVersion);
-
-
-    @GET("apk/stores/{store_id}/version_type/{version_type}/validate")
-    Call<ResponseBody> validateApkVersion(@Path("store_id") Long storeId, @Path("version_type") String validationTYpe,
-                                          @Query("current_version") Long currentVersion);
+    @GET("apk/stores/{store_id}/{app_name}/{version_type}/validate")
+    Call<ResponseBody> validateApkVersion(@Path("store_id") Long storeId, @Path("app_name") String appName,
+                                          @Path("version_type") String validationTYpe, @Query("current_version") Long currentVersion);
 
     @GET("devices/{deviceId}/whitelist")
     Call<Boolean> isDeviceWhitelisted(@Path("deviceId") String deviceId);
--- src/main/java/it/gruppopam/app_common/network/interceptor/BaseHeadersInterceptor.java
+++ src/main/java/it/gruppopam/app_common/network/interceptor/BaseHeadersInterceptor.java
@@ -12,6 +12,8 @@ public abstract class BaseHeadersInterceptor implements Interceptor {
     public static final String DEVICE_ID_HEADER = "X-Device-Id";
     public static final String USERNAME_HEADER = "X-User-Id";
     public static final String CLIENT_HEADER = "X-Client-Id";
+    public static final String VERSION_ID_HEADER = "X-Version-Id";
+    public static final String VERSION_TYPE_HEADER = "X-Version-Type";
     public static final String REQUEST_HEADER = "X-Request-Id";
     public static final String STORE_ID_HEADER = "X-Store-Id";
     public static final String UNKNOWN_DEVICE_ID = "UNKNOWN";
@@ -26,13 +28,15 @@ public abstract class BaseHeadersInterceptor implements Interceptor {
         this.cliendHeaderVal = cliendHeaderVal;
     }
 
-    protected Request.Builder buildRequestInterceptor(Request originalRequest, String uuid, String deviceId, String username, Long storeId) {
+    protected Request.Builder buildRequestInterceptor(Request originalRequest, String uuid, String deviceId, String username, Long storeId, String versionId, String versionType) {
         Request.Builder builder = originalRequest.newBuilder()
             .header(REQUEST_HEADER, uuid)
             .header(USERNAME_HEADER, username)
             .header(CLIENT_HEADER, cliendHeaderVal)
             .header(DEVICE_ID_HEADER, deviceId != null ? deviceId : UNKNOWN_DEVICE_ID)
             .header(STORE_ID_HEADER, String.valueOf(storeId))
+            .header(VERSION_ID_HEADER, versionId)
+            .header(VERSION_TYPE_HEADER, versionType)
             .method(originalRequest.method(), originalRequest.body());
 
         return builder;
